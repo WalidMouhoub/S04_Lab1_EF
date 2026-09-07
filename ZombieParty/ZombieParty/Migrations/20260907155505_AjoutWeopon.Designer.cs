e409653c06de1902d4b0b245afcb5f80857e554e
Walid Mouhoub <walidmouhoub115@gmail.com>
2026-09-07 11:56:13 -0400

    FCT Weapon
    Création de la table Weapon

--- ZombieParty/ZombieParty/Migrations/20260907155505_AjoutWeopon.Designer.cs
+++ ZombieParty/ZombieParty/Migrations/20260907155505_AjoutWeopon.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZombieParty.Models;
 
@@ -11,9 +12,11 @@ using ZombieParty.Models;
 namespace ZombieParty.Migrations
 {
     [DbContext(typeof(ZombiePartyDbContext))]
-    partial class ZombiePartyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260907155505_AjoutWeopon")]
+    partial class AjoutWeopon
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ZombieParty/ZombieParty/Models/ZombiePartyDbContext.cs
+++ ZombieParty/ZombieParty/Models/ZombiePartyDbContext.cs
@@ -12,6 +12,7 @@ namespace ZombieParty.Models
         public DbSet<Zombie> Zombies { get; set; }
         public DbSet<ZombieType> ZombieTypes { get; set; }
         public DbSet<HuntingLog> HuntingLogs { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
 
     }
 }
